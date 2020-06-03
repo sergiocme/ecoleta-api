bc416be7b8d106597b9b70de9dca724a6c561572
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-06-03 12:35:19 -0300

    Implement list users and search by name and index as id

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,47 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (resquest, response) => {
-  
-  response.json([
-    'Sérgio',
-    'Gabriel',
-    'Pedro'
-  ]);
+const users = [
+  {
+    name: 'Sérgio Carvalho',
+    email: 'sergiocarvalho.m@outlook.com',
+  },
+  {
+    name: 'Isabela Carvalho',
+    email: 'isabela.carvalho@batata.com',
+  },
+  {
+    name: 'Rodrigo Maia',
+    email: 'rodrigo.maia@batata.com',
+  },
+];
+
+app.get('/users', (request, response) => {
+  const name = request.query.name ? String(request.query.name).toLowerCase() : request.query.name;
+
+  const searchedUsers = (name && name.length) ? users.filter((user) => {
+    const caseInsensitiveName = user.name.toLowerCase();
+    
+    return (caseInsensitiveName.includes(name));
+  }) : users;
+
+  return response.json(searchedUsers);
+});
+
+app.get('/users:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  return response.json(users[id]);
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego Fernandes',
+    email: 'diegofernandes@rocketseat.com.br',
+  }
+
+  users.push(user);
+  return response.send();
 });
 
 app.listen(3333);
