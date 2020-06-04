a4af2024ff570407f98fe955a6b3f91e0ca72064
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-06-03 23:28:45 -0300

    Copy assets files and set as static route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import path from 'path';
 
 import usersRoutes from './users';
 
 const router = Router();
 
-router.use(usersRoutes);
+router.use('/assets', express.static(path.resolve(__dirname, '..', '..', 'assets')));
 
 export default router;
